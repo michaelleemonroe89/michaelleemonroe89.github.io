a6e367cc2176d402a3e7fe5746638ef76a61ce81
michaelleemonroe89 <michaelleemonroe@gmail.com>
2025-09-01 19:54:14 -0400

    Delete udacity_projects directory

--- udacity_projects/supervised_and_unsupervised_learning/find_donors_for_charityml/nav.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Navigation bar for project subpages
-function nav_links($back = '../index.php') {
-    return '<nav><a href="/index.php">Home</a> | <a href="/udacity_projects/index.php">Udacity Projects</a></nav>';
-}
-?>
